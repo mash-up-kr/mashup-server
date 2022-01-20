fb92fbd61920d62595a61211b7797eb1e0079035
Haeseong Jeon <seong0428@gmail.com>
2022-01-20 21:03:55 +0900

    feat: health check 추가

---
+++ mashup-admin/src/main/java/kr/mashup/branding/ui/HelloController.java
@@ -0,0 +1,12 @@
+package kr.mashup.branding.ui;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, Mashup-admin";
+    }
+}
