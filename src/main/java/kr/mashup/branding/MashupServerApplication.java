49c24f8e3ed28f714b7c7a9690155d6ced66d5e4
Haeseong Jeon <seong0428@gmail.com>
2022-01-20 19:56:59 +0900

    Initial Commit

---
+++ src/main/java/kr/mashup/branding/MashupServerApplication.java
@@ -0,0 +1,13 @@
+package kr.mashup.branding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MashupServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MashupServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/mashup/branding/MashupServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.mashup.branding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MashupServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
