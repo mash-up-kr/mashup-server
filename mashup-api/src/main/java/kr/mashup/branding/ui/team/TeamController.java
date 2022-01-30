7680d5423adc5a0afc4dd680a49232f7afaba78e
Haeseong Jeon <seong0428@gmail.com>
2022-01-30 16:11:36 +0900

    feat: Team 목록 조회

--- mashup-api/src/main/java/kr/mashup/branding/ui/team/TeamController.java
+++ mashup-api/src/main/java/kr/mashup/branding/ui/team/TeamController.java
@@ -1,19 +1,28 @@
 package kr.mashup.branding.ui.team;
 
-import io.swagger.annotations.ApiOperation;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
-import java.util.List;
+import io.swagger.annotations.ApiOperation;
+import kr.mashup.branding.facade.team.TeamFacadeService;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1/teams")
+@RequiredArgsConstructor
 public class TeamController {
+    private final TeamFacadeService teamFacadeService;
+    private final TeamAssembler teamAssembler;
+
     @ApiOperation("팀 목록 조회")
     @GetMapping
     public List<TeamResponse> getTeams() {
-        return Collections.emptyList();
+        return teamFacadeService.getTeams().stream()
+            .map(teamAssembler::toTeamResponse)
+            .collect(Collectors.toList());
     }
 }
--- mashup-domain/src/main/java/kr/mashup/branding/domain/team/Team.java
+++ mashup-domain/src/main/java/kr/mashup/branding/domain/team/Team.java
@@ -1,16 +1,24 @@
 package kr.mashup.branding.domain.team;
 
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 
 @Entity
+@Getter
+@ToString
+@EqualsAndHashCode(of = "teamId")
 @EntityListeners(AuditingEntityListener.class)
 public class Team {
     @Id
