fb70d57577aed7ff93966123da20cd0e356d5057
전해성 <seong0428@gmail.com>
2022-02-01 22:59:04 +0900

    지원서, 질문 필드 추가 (#11)
    * feat: applicationForm 이름 추가
    
    * feat: 질문 타입 추가
    
    * feat: 필수 질문인지 여부 필드 추가
    
    * feat: api 응답 변경

--- mashup-domain/src/main/java/kr/mashup/branding/domain/application/form/Question.java
+++ mashup-domain/src/main/java/kr/mashup/branding/domain/application/form/Question.java
@@ -1,14 +1,22 @@
 package kr.mashup.branding.domain.application.form;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.ToString;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.*;
-import java.time.LocalDateTime;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 
 @Entity
 @Getter
@@ -31,6 +39,17 @@ public class Question {
      */
     private Integer properSize;
 
+    /**
+     * 필수 질문 여부
+     */
+    private Boolean required;
+
+    /**
+     * 질문 종류 (단답형, 장문형)
+     */
+    @Enumerated(EnumType.STRING)
+    private QuestionType questionType;
+
     @CreatedDate
     private LocalDateTime createdAt;
 
@@ -41,6 +60,8 @@ public class Question {
         Question question = new Question();
         question.content = createQuestionVo.getContent();
         question.properSize = createQuestionVo.getProperSize();
+        question.required = createQuestionVo.getRequired();
+        question.questionType = createQuestionVo.getQuestionType();
         return question;
     }
 }
