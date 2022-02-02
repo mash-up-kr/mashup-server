a62215e7e92a310a135f8b093f43c14dc6508f29
devheee <heee7897@gmail.com>
2022-02-03 01:10:22 +0900

    feat: Applicant 도메인 코드 추가

---
+++ mashup-domain/src/main/java/kr/mashup/branding/domain/applicant/ApplicantRepository.java
@@ -0,0 +1,6 @@
+package kr.mashup.branding.domain.applicant;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplicantRepository extends JpaRepository<Applicant, Long> {
+}
---
+++ mashup-domain/src/main/java/kr/mashup/branding/domain/applicant/ApplicantStatus.java
@@ -0,0 +1,5 @@
+package kr.mashup.branding.domain.applicant;
+
+public enum ApplicantStatus {
+    ACTIVE, WITHDRAWAL
+}
