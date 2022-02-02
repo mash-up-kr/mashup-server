d323be0e8df6c2485c782ee31359448003a2fb12
garden.lee <khjvvv7@gmail.com>
2022-02-03 00:12:48 +0900

    feat: AdminMember getById

--- mashup-domain/src/main/java/kr/mashup/branding/domain/adminmember/AdminMemberService.java
+++ mashup-domain/src/main/java/kr/mashup/branding/domain/adminmember/AdminMemberService.java
@@ -3,4 +3,5 @@ package kr.mashup.branding.domain.adminmember;
 public interface AdminMemberService {
     AdminMember signUp(AdminMemberVo adminMemberVo);
     AdminMember signIn(AdminMemberSignInVo adminMemberSignInVo);
+    AdminMember getByAdminMemberId(Long adminMemberId);
 }
--- mashup-domain/src/main/java/kr/mashup/branding/domain/adminmember/AdminMemberServiceImpl.java
+++ mashup-domain/src/main/java/kr/mashup/branding/domain/adminmember/AdminMemberServiceImpl.java
@@ -1,11 +1,12 @@
 package kr.mashup.branding.domain.adminmember;
 
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
 import kr.mashup.branding.domain.team.Team;
 import kr.mashup.branding.domain.team.TeamService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
 
 @RequiredArgsConstructor
 @Transactional
@@ -21,10 +22,10 @@ public class AdminMemberServiceImpl implements AdminMemberService {
 
         //TODO: providerUserId 검증
         AdminMember adminMember = AdminMember.of(
-                adminMemberVo.getProviderUserId(),
-                adminMemberVo.getName(),
-                team,
-                adminMemberVo.getDescription()
+            adminMemberVo.getProviderUserId(),
+            adminMemberVo.getName(),
+            team,
+            adminMemberVo.getDescription()
         );
 
         return adminMemberRepository.save(adminMember);
@@ -34,6 +35,12 @@ public class AdminMemberServiceImpl implements AdminMemberService {
     public AdminMember signIn(AdminMemberSignInVo adminMemberSignInVo) {
         //TODO: providerUserId 검증
         return adminMemberRepository.findByProviderUserId(adminMemberSignInVo.getProviderUserId())
-                .orElseThrow(AdminMemberNotFoundException::new);
+            .orElseThrow(AdminMemberNotFoundException::new);
+    }
+
+    @Override
+    public AdminMember getByAdminMemberId(Long adminMemberId) {
+        return adminMemberRepository.findById(adminMemberId)
+            .orElseThrow(AdminMemberNotFoundException::new);
     }
 }
