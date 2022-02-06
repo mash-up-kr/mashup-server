12676ce9d9d01b07d8d1428000e9655f536b88fa
Haeseong Jeon <seong0428@gmail.com>
2022-02-07 00:20:48 +0900

    feat: exception handler 추가

---
+++ mashup-admin/src/main/java/kr/mashup/branding/ui/AdminControllerAdvice.java
@@ -0,0 +1,20 @@
+package kr.mashup.branding.ui;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class AdminControllerAdvice {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ApiResponse<?> handleException(Exception e) {
+        log.error("handleException", e);
+        return ApiResponse.failure("FAILURE", "실패");
+    }
+}
