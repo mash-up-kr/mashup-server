e57b39f6141cba88471bb27d8a39a2277d8a8de6
Haeseong Jeon <seong0428@gmail.com>
2022-02-09 00:24:12 +0900

    test: 개인정보처리방침 동의여부 테스트케이스 추가

--- mashup-domain/src/main/java/kr/mashup/branding/domain/application/Application.java
+++ mashup-domain/src/main/java/kr/mashup/branding/domain/application/Application.java
@@ -100,7 +100,6 @@ public class Application {
             .map(Answer::empty)
             .collect(Collectors.toList());
         application.answers.addAll(answers);
-        application.privacyPolicyAgreed = false;
         return application;
     }
 
@@ -111,7 +110,7 @@ public class Application {
         Assert.notNull(updateApplicationVo, "'updateApplicationVo' must not be null");
         if (Boolean.TRUE != updateApplicationVo.getPrivacyPolicyAgreed()) {
             throw new PrivacyPolicyNotAgreedException("'privacyPolicyAgreed' must be true. privacyPolicyAgreed: "
-                + updateApplicationVo.getPrivacyPolicyAgreed().toString());
+                + updateApplicationVo.getPrivacyPolicyAgreed());
         }
         Map<Long, AnswerRequestVo> questionAnswerMap = updateApplicationVo.getAnswerRequestVoList()
             .stream()
@@ -129,9 +128,9 @@ public class Application {
      * 지원서 제출
      */
     void submit() {
-        if (this.getPrivacyPolicyAgreed()) {
+        if (Boolean.TRUE != this.privacyPolicyAgreed) {
             throw new PrivacyPolicyNotAgreedException("'privacyPolicyAgreed' must be true. privacyPolicyAgreed: "
-                + this.privacyPolicyAgreed.toString());
+                + this.privacyPolicyAgreed);
         }
         try {
             status = status.submit();
--- mashup-domain/src/main/java/kr/mashup/branding/domain/application/ApplicationServiceImpl.java
+++ mashup-domain/src/main/java/kr/mashup/branding/domain/application/ApplicationServiceImpl.java
@@ -9,10 +9,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
+import kr.mashup.branding.domain.MashupScheduleService;
 import kr.mashup.branding.domain.applicant.Applicant;
 import kr.mashup.branding.domain.applicant.ApplicantNotFoundException;
 import kr.mashup.branding.domain.applicant.ApplicantService;
-import kr.mashup.branding.domain.MashupSchedule;
 import kr.mashup.branding.domain.application.form.ApplicationForm;
 import kr.mashup.branding.domain.application.form.ApplicationFormNotFoundException;
 import kr.mashup.branding.domain.application.form.ApplicationFormService;
@@ -31,6 +31,7 @@ public class ApplicationServiceImpl implements ApplicationService {
     private final ApplicationFormService applicationFormService;
     private final TeamService teamService;
     private final ApplicantService applicantService;
+    private final MashupScheduleService mashupScheduleService;
 
     // get or create
     // TODO: 모르겠고 teamId 줄테니 다내놔! 에 대해서 고민해보기
@@ -116,7 +117,7 @@ public class ApplicationServiceImpl implements ApplicationService {
      * 지원서 생성, 수정, 제출 가능한 시각인지 검증
      */
     private void validateDate(LocalDateTime localDateTime) {
-        if (!MashupSchedule.isRecruitAvailable(localDateTime)) {
+        if (!mashupScheduleService.isRecruitAvailable(localDateTime)) {
             throw new IllegalArgumentException("지원서 제출 기간이 아닙니다. ");
         }
     }
