a6066778cb9bbf8f5b16c19aa941bb38f1e814a5
devheee <heee7897@gmail.com>
2022-02-12 02:26:10 +0900

    fix: 불필요한 주석 삭제

--- mashup-api/src/main/java/kr/mashup/branding/config/security/TokenMissingException.java
+++ mashup-api/src/main/java/kr/mashup/branding/config/security/TokenMissingException.java
@@ -2,7 +2,6 @@ package kr.mashup.branding.config.security;
 
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-// 클래스 위치 코멘트
 public class TokenMissingException extends UsernameNotFoundException {
     public TokenMissingException(String message) {
         super(message);
