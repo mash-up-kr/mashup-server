41a480619a8f399c5bdae42a09c92567c4789be6
전해성 <seong0428@gmail.com>
2022-02-12 06:37:46 +0900

    config: production 환경에서 swagger 사용하지 않음 (#32)

--- mashup-api/src/main/java/kr/mashup/branding/config/swagger/SwaggerConfig.java
+++ mashup-api/src/main/java/kr/mashup/branding/config/swagger/SwaggerConfig.java
@@ -11,11 +11,9 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Profile("swagger")
 @ConditionalOnWebApplication
-@EnableSwagger2
 @Configuration
 class SwaggerConfig {
 
