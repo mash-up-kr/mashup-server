11b56d388b2950fd6bbb46a23fcaf330f507b61f
Haeseong Jeon <seong0428@gmail.com>
2022-02-13 20:29:10 +0900

    feat: adminMember 가입 API 삭제

--- mashup-admin/src/main/java/kr/mashup/branding/ui/adminmember/AdminMemberController.java
+++ mashup-admin/src/main/java/kr/mashup/branding/ui/adminmember/AdminMemberController.java
@@ -5,9 +5,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import kr.mashup.branding.domain.adminmember.AdminMember;
 import kr.mashup.branding.domain.adminmember.AdminMemberSignInVo;
-import kr.mashup.branding.domain.adminmember.AdminMemberVo;
 import kr.mashup.branding.facade.adminmember.AdminMemberFacadeService;
 import kr.mashup.branding.facade.adminmember.SignInVo;
 import kr.mashup.branding.ui.ApiResponse;
@@ -21,17 +19,8 @@ public class AdminMemberController {
     private final AdminMemberAssembler adminMemberAssembler;
     private final AdminMemberFacadeService adminMemberFacadeService;
 
-    @PostMapping("/sign-up")
-    public ApiResponse<SignUpResponse> signup(
-        @RequestBody SignUpRequest signUpRequest
-    ) {
-        AdminMemberVo adminMemberVo = adminMemberAssembler.toAdminMemberVo(signUpRequest);
-        AdminMember adminMember = adminMemberFacadeService.signUp(adminMemberVo);
-        return ApiResponse.success(adminMemberAssembler.toSignUpResponse(adminMember));
-    }
-
     @PostMapping("/sign-in")
-    public ApiResponse<SignInResponse> signin(
+    public ApiResponse<SignInResponse> signIn(
         @RequestBody SignInRequest signInRequest
     ) {
         AdminMemberSignInVo adminMemberSignInVo = adminMemberAssembler.toAdminMemberSignInVo(signInRequest);
