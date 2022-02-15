ab3d05f4e16db97a7650bd1ae6ae03b2d88f0cca
Haeseong Jeon <seong0428@gmail.com>
2022-02-15 22:57:21 +0900

    feat: 사용하지 않는 필드 제거

--- mashup-admin/src/main/java/kr/mashup/branding/ui/notification/sms/SmsRequestDetailResponse.java
+++ mashup-admin/src/main/java/kr/mashup/branding/ui/notification/sms/SmsRequestDetailResponse.java
@@ -21,12 +21,6 @@ public class SmsRequestDetailResponse {
     @ApiModelProperty(value = "발송 상태(성공, 실패)", example = "SUCCESS")
     private SmsNotificationStatus status;
 
-    @ApiModelProperty(value = "수신자 이름", example = "홍길동")
-    private String recipientName;
-
-    @ApiModelProperty(value = "수신자 전화번호", example = "01000000000")
-    private String recipientPhoneNumber;
-
     @ApiModelProperty(value = "수신자 지원 플랫폼")
     private TeamResponse team;
 }
