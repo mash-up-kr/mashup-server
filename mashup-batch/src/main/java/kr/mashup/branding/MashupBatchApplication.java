97984bf3ec568dbb35d3ed08dadddeaf894e2038
Haeseong Jeon <seong0428@gmail.com>
2022-02-18 19:22:13 +0900

    feat: 배치 실행시 parameter 추가

--- mashup-batch/src/main/java/kr/mashup/branding/MashupBatchApplication.java
+++ mashup-batch/src/main/java/kr/mashup/branding/MashupBatchApplication.java
@@ -1,11 +1,19 @@
 package kr.mashup.branding;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MashupBatchApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MashupBatchApplication.class, args);
+        System.setProperty("spring.config.name", "application,batch");
+        List<String> argList = new ArrayList<>(Arrays.asList(args));
+        argList.add("startTime=" + LocalDateTime.now());
+        SpringApplication.run(MashupBatchApplication.class, argList.toArray(new String[0]));
     }
 }
