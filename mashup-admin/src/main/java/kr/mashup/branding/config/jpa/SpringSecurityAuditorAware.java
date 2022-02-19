ebbcb0b8489ce53964bcd8f06b75083dedd4414c
Haeseong Jeon <seong0428@gmail.com>
2022-02-20 01:04:07 +0900

    fix: auditor 값에 adminMemberId 가 잘못 입력되는 현상 해결

--- mashup-admin/src/main/java/kr/mashup/branding/config/jpa/SpringSecurityAuditorAware.java
+++ mashup-admin/src/main/java/kr/mashup/branding/config/jpa/SpringSecurityAuditorAware.java
@@ -1,50 +1,27 @@
 package kr.mashup.branding.config.jpa;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-import kr.mashup.branding.domain.adminmember.AdminMember;
-import kr.mashup.branding.domain.adminmember.AdminMemberService;
-import kr.mashup.branding.domain.adminmember.Position;
-
 public class SpringSecurityAuditorAware implements AuditorAware<String> {
-
-    private static final String ANONYMOUS_USER = "anonymousUser";
-
-    private AdminMemberService adminMemberService;
-    private List<SimpleGrantedAuthority> authoritySet;
-
-    private SpringSecurityAuditorAware() {
-    }
-
-    public SpringSecurityAuditorAware(AdminMemberService adminMemberService) {
-        this.adminMemberService = adminMemberService;
-        authoritySet = Arrays.stream(Position.values())
-            .map(position -> new SimpleGrantedAuthority(position.name()))
-            .collect(Collectors.toList());
-    }
 
     @Override
     public Optional<String> getCurrentAuditor() {
         return Optional.ofNullable(SecurityContextHolder.getContext())
             .map(SecurityContext::getAuthentication)
             .map(authentication -> {
-                if (!authoritySet.retainAll(authentication.getAuthorities())) {
-                    AdminMember adminMember = adminMemberService.getByAdminMemberId(
-                        (Long)authentication.getPrincipal());
-                    return adminMember.getPosition().name();
+                if (authentication.getPrincipal() == null) {
+                    return null;
                 }
-                if (authentication.getPrincipal() != null) {
-                    return authentication.getPrincipal().toString();
-                }
-                return ANONYMOUS_USER;
+                return authentication.getAuthorities()
+                    .stream()
+                    .findFirst()
+                    .map(GrantedAuthority::getAuthority)
+                    .orElse(null);
             });
     }
 }
