2121772ca6e6c5f700d39e28c534df1992a12250
Haeseong Jeon <seong0428@gmail.com>
2022-02-20 01:06:06 +0900

    fix: NPE 방어코드 추가

--- mashup-admin/src/main/java/kr/mashup/branding/config/jpa/SpringSecurityAuditorAware.java
+++ mashup-admin/src/main/java/kr/mashup/branding/config/jpa/SpringSecurityAuditorAware.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.CollectionUtils;
 
 public class SpringSecurityAuditorAware implements AuditorAware<String> {
 
@@ -15,6 +16,9 @@ public class SpringSecurityAuditorAware implements AuditorAware<String> {
             .map(SecurityContext::getAuthentication)
             .map(authentication -> {
                 if (authentication.getPrincipal() == null) {
+                    return null;
+                }
+                if (CollectionUtils.isEmpty(authentication.getAuthorities())) {
                     return null;
                 }
                 return authentication.getAuthorities()
