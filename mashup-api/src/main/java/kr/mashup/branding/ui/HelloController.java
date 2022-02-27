1023c285bff0229e23a1b04ee8e3e0c171dfee3f
Haeseong Jeon <seong0428@gmail.com>
2022-02-27 12:04:02 +0900

    refactor

--- mashup-api/src/main/java/kr/mashup/branding/ui/HelloController.java
+++ mashup-api/src/main/java/kr/mashup/branding/ui/HelloController.java
@@ -3,9 +3,6 @@ package kr.mashup.branding.ui;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 @RestController
 public class HelloController {
     @GetMapping("/hello")
