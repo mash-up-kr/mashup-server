5e6ce735ba9d840af1713ecde2bf29234626cb50
choiminseong <choi-minseong@naverz-corp.com>
2022-05-17 00:39:29 +0900

    TestController -> PingController

--- mashup-member/src/main/java/kr/mashup/branding/ui/PingController.java
+++ mashup-member/src/main/java/kr/mashup/branding/ui/PingController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
+public class PingController {
 	@GetMapping("/ping")
 	public boolean ping() {
 		return true;
