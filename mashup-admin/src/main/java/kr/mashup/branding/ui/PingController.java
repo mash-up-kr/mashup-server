46804b22f950f3b227abbbf6d821e3d0f92a2f6b
choiminseong <choi-minseong@naverz-corp.com>
2022-05-20 04:07:56 +0900

    cicd

--- mashup-admin/src/main/java/kr/mashup/branding/ui/PingController.java
+++ mashup-admin/src/main/java/kr/mashup/branding/ui/PingController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class PingController {
 	@GetMapping("/ping")
 	public String ping() {
-		return "mashup-adminsoo";
+		return "MashUp-Adminsoo!";
 	}
 }
