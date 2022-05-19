c2f0fbaecca98d8d4b652fe6102a8f64b5b23fe8
choiminseong <choi-minseong@naverz-corp.com>
2022-05-20 04:25:17 +0900

    cicd

--- docker/deploy/recruit/prod/deploy-recruit.sh
+++ docker/deploy/recruit/prod/deploy-recruit.sh
@@ -2,7 +2,7 @@
 
 # BLUE가 실행중인지 확인
 APP_NAME=mashup-recruit
-EXIST_BLUE=$(docker-compose -p mashup-recruit-blue -f docker-compose.blue.yml ps | grep Up)
+EXIST_BLUE=$(docker-compose -p mashup-recruit-blue -f docker-compose.blue.yml ps | grep mashup-recruit-blue)
 
 if [ -z "${EXIST_BLUE}" ] # -z는 문자열 길이가 0이면 true.BLUE가 실행중이면 false
 then
