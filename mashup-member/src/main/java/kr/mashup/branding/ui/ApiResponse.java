70e3942b054a06f75db97e64053e48bfff632c73
jong yoon <dhorlawhddbs@naver.com>
2022-06-14 23:30:01 +0900

    feat: 멤버 모듈 에러 전역 처리 및 표준 응답 처리 추가

---
+++ mashup-member/src/main/java/kr/mashup/branding/ui/ApiResponse.java
@@ -0,0 +1,102 @@
+package kr.mashup.branding.ui;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import kr.mashup.branding.domain.ResultCode;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Getter
+@ToString
+public class ApiResponse<T> {
+    private final ResultCode code;
+    private final String message;
+    private final T data;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private final PageResponse page;
+
+    private ApiResponse(ResultCode code, String message, T data, PageResponse page) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+        this.page = page;
+    }
+
+    public static <T> ApiResponse<T> success() {
+        return new ApiResponse<>(
+            ResultCode.SUCCESS,
+            ResultCode.SUCCESS.getMessage(),
+            null,
+            null
+        );
+    }
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(
+            ResultCode.SUCCESS,
+            ResultCode.SUCCESS.getMessage(),
+            data,
+            null
+        );
+    }
+
+    public static <T> ApiResponse<List<T>> success(List<T> data) {
+        return new ApiResponse<>(
+            ResultCode.SUCCESS,
+            ResultCode.SUCCESS.getMessage(),
+            data,
+            null
+        );
+    }
+
+    public static <T> ApiResponse<List<T>> success(Page<T> data) {
+        return new ApiResponse<>(
+            ResultCode.SUCCESS,
+            ResultCode.SUCCESS.getMessage(),
+            data.getContent(),
+            PageResponse.from(data)
+        );
+    }
+
+    public static <T> ApiResponse<T> failure(ResultCode resultCode) {
+        return new ApiResponse<>(
+            resultCode,
+            resultCode.getMessage(),
+            null,
+            null
+        );
+    }
+
+    public static <T> ApiResponse<T> failure(ResultCode resultCode, String message) {
+        return new ApiResponse<>(
+            resultCode,
+            message,
+            null,
+            null
+        );
+    }
+
+    @Getter
+    @ToString
+    public static class PageResponse {
+        private final int number;
+        private final int size;
+        private final int totalCount;
+
+        private PageResponse(int number, int size, int totalCount) {
+            this.number = number;
+            this.size = size;
+            this.totalCount = totalCount;
+        }
+
+        public static PageResponse from(Page<?> page) {
+            return new PageResponse(
+                page.getNumber(),
+                page.getSize(),
+                (int)page.getTotalElements()
+            );
+        }
+    }
+}
