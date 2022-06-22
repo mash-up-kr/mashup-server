337b1ccb04e33e1fa13c64fcf9a403fd900723c1
hocaron <kkannu0407@gmail.com>
2022-06-23 00:12:49 +0900

    feat: content 생성 repository 및 service 생성

--- mashup-domain/src/main/java/kr/mashup/branding/domain/content/Content.java
+++ mashup-domain/src/main/java/kr/mashup/branding/domain/content/Content.java
@@ -1,11 +1,14 @@
 package kr.mashup.branding.domain.content;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.sun.istack.NotNull;
+
 import kr.mashup.branding.domain.BaseEntity;
 import kr.mashup.branding.domain.event.Event;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 import org.springframework.util.Assert;
 
 import javax.persistence.Entity;
@@ -21,26 +24,26 @@ public class Content extends BaseEntity {
     @NotNull
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "event_id")
+    @JsonIgnore
     private Event event;
 
     @NotNull
     private String content;
 
-    public static Content of(Event event, String content){
+    public static Content of(Event event, String content) {
         return new Content(event, content);
     }
 
-    private Content(Event event, String content){
+    private Content(Event event, String content) {
 
         this.event = event;
         event.addContent(this);
         this.content = content;
     }
 
-    public void changeContent(String newContent){
+    public void changeContent(String newContent) {
         Assert.hasText(newContent, "");
         this.content = newContent;
     }
-
 
 }
