8fc9e7ffb1e1d6e12f5df9440db30c55a203a549
hocaron <kkannu0407@gmail.com>
2022-06-23 00:13:14 +0900

    feat: content controller 및 dto 생성

---
+++ mashup-member/src/main/java/kr/mashup/branding/ui/content/ContentController.java
@@ -0,0 +1,34 @@
+package kr.mashup.branding.ui.content;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiOperation;
+import kr.mashup.branding.domain.content.Content;
+import kr.mashup.branding.domain.content.ContentCreateVo;
+import kr.mashup.branding.service.content.ContentService;
+import kr.mashup.branding.ui.ApiResponse;
+import kr.mashup.branding.ui.content.request.ContentCreateRequest;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/contents")
+@RestController
+public class ContentController {
+
+    private final ContentService contentService;
+
+    @ApiOperation("이벤트 내용 생성")
+    @PostMapping()
+    public ApiResponse<Content> create(
+        @RequestBody ContentCreateRequest contentCreateRequest
+    ) {
+        Content content = contentService.create(
+            ContentCreateVo.of(
+                contentCreateRequest.getContent(), contentCreateRequest.getEventId()
+            ));
+        return ApiResponse.success(content);
+    }
+}
