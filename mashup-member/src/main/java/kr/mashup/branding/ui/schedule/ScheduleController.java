51cf30a9f36ddaee3853b9f75794ec68ef56d14f
hocaron <kkannu0407@gmail.com>
2022-06-25 20:25:45 +0900

    feat: schedule 조회 서비스 및 컨트롤러 생성

--- mashup-member/src/main/java/kr/mashup/branding/ui/schedule/ScheduleController.java
+++ mashup-member/src/main/java/kr/mashup/branding/ui/schedule/ScheduleController.java
@@ -1,7 +1,12 @@
 package kr.mashup.branding.ui.schedule;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.validation.Valid;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,21 +26,38 @@ import lombok.RequiredArgsConstructor;
 @RestController
 public class ScheduleController {
 
-    private final ScheduleService scheduleService;
-    private final ScheduleAssembler scheduleAssembler;
+	private final ScheduleService scheduleService;
 
-    @ApiOperation("스케줄 생성")
-    @PostMapping()
-    public ApiResponse<ScheduleResponse> create(
-         @Valid @RequestBody ScheduleCreateRequest scheduleCreateRequest
-    ) {
-        Schedule schedule = scheduleService.create(
-            ScheduleCreateVo.of(
-                scheduleCreateRequest.getName(),
-                scheduleCreateRequest.getStartedAt(),
-                scheduleCreateRequest.getEndedAt(),
-                scheduleCreateRequest.getGenerationId())
-        );
-        return ApiResponse.success(scheduleAssembler.toScheduleResponse(schedule));
-    }
+	@ApiOperation("스케줄 생성")
+	@PostMapping()
+	public ApiResponse<ScheduleResponse> create(
+		@Valid @RequestBody ScheduleCreateRequest scheduleCreateRequest
+	) {
+		Schedule schedule = scheduleService.create(
+			ScheduleCreateVo.of(
+				scheduleCreateRequest.getName(),
+				scheduleCreateRequest.getStartedAt(),
+				scheduleCreateRequest.getEndedAt(),
+				scheduleCreateRequest.getGenerationId())
+		);
+		return ApiResponse.success(ScheduleResponse.of(schedule));
+	}
+
+	@ApiOperation("스케줄 조회")
+	@GetMapping("/{id}")
+	public ApiResponse<ScheduleResponse> getById(@PathVariable Long id) {
+		Schedule schedule = scheduleService.getByIdOrThrow(id);
+		return ApiResponse.success(ScheduleResponse.of(schedule));
+	}
+
+	@ApiOperation("기수로 스케줄 조회")
+	@GetMapping("generations/{generationNumber}")
+	public ApiResponse<List<ScheduleResponse>> getByGenerationNumber(@PathVariable Integer generationNumber) {
+		List<Schedule> scheduleList = scheduleService.getByGenerationNumber(generationNumber);
+		return ApiResponse.success(
+			scheduleList.stream()
+				.map(ScheduleResponse::of)
+				.collect(Collectors.toList())
+		);
+	}
 }
