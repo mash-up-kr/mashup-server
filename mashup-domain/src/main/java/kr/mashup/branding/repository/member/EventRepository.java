04a7130c357bfecac89d25cc4885726745b0be93
jong yoon <dhorlawhddbs@naver.com>
2022-06-25 21:48:28 +0900

    remove: 파일 제거

--- mashup-domain/src/main/java/kr/mashup/branding/repository/member/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.mashup.branding.repository.member;
-
-import kr.mashup.branding.domain.event.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
