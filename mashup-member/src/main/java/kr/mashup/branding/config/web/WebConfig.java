d01eb740b6e96955083cc5d88e6d6567e663fc7e
jong yoon <dhorlawhddbs@naver.com>
2022-07-02 23:56:06 +0900

    feat: PathVariable에서 Enum의 소문자를 받을 수 있도록 적용

--- mashup-member/src/main/java/kr/mashup/branding/config/web/WebConfig.java
+++ mashup-member/src/main/java/kr/mashup/branding/config/web/WebConfig.java
@@ -1,7 +1,9 @@
 package kr.mashup.branding.config.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.convert.ApplicationConversionService;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,9 +13,15 @@ import java.util.List;
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
     private final MemberAuthArgumentResolver memberAuthArgumentResolver;
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(memberAuthArgumentResolver);
         WebMvcConfigurer.super.addArgumentResolvers(resolvers);
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        ApplicationConversionService.configure(registry);
     }
 }
