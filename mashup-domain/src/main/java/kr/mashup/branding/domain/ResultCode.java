13b1940c9fc2f2a741810b8b1fc29bab4cc4852a
JOngnan <dirlawhddbs@gmail.com>
2022-07-04 22:58:33 +0900

    feat: swagger에 에러 코드 관련 설명 넣기

--- mashup-domain/src/main/java/kr/mashup/branding/domain/ResultCode.java
+++ mashup-domain/src/main/java/kr/mashup/branding/domain/ResultCode.java
@@ -75,7 +75,6 @@ public enum ResultCode {
     // AttendanceCode (출석 코드)
     ATTENDANCE_CODE_DUPLICATED("이미 사용된 코드입니다."),
     ATTENDANCE_CODE_NOT_FOUND("출석 코드가 존재하지 않습니다."),
-    ATTENDANCE_CODE_EMPTY("출석 코드가 만들어지지 않았습니다."),
 
     // Attendance (출석)
     ATTENDANCE_NOT_FOUND("출석 정보가 존재하지 않습니다."),
--- mashup-member/src/main/java/kr/mashup/branding/config/swagger/SwaggerConfig.java
+++ mashup-member/src/main/java/kr/mashup/branding/config/swagger/SwaggerConfig.java
@@ -1,19 +1,28 @@
 package kr.mashup.branding.config.swagger;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collections;
+import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.RestController;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -21,6 +30,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 @Profile("swagger")
 @ConditionalOnWebApplication
 @Configuration
+@Slf4j
 class SwaggerConfig {
 
     @Bean
@@ -31,6 +41,8 @@ class SwaggerConfig {
             .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
             .paths(PathSelectors.ant("/api/**"))
             .build()
+            .useDefaultResponseMessages(false)
+            .apiInfo(apiInfo())
             .directModelSubstitute(Pageable.class, SwaggerPageableRequest.class)
             .securitySchemes(Collections.singletonList(apiKey()))
             .securityContexts(Collections.singletonList(securityContext()));
@@ -51,5 +63,25 @@ class SwaggerConfig {
 
     private ApiKey apiKey() {
         return new ApiKey("Bearer {accessToken}", "Authorization", "header");
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("Mash-Up 출석체크 API 문서")
+            .description(getMainDescription())
+            .build();
+    }
+
+    private String getMainDescription() {
+        StringBuilder sb = new StringBuilder();
+        try {
+            ClassPathResource resource = new ClassPathResource("static/mainDescription.html");
+            Path path = Paths.get(resource.getURI());
+            List<String> content = Files.readAllLines(path);
+            content.forEach(sb::append);
+        } catch (IOException e) {
+            log.error("Main Description Error: {}", e.getMessage());
+        }
+        return sb.toString();
     }
 }
