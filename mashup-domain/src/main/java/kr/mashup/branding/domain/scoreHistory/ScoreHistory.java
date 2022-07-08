9a58c7de3587ad22d5584b6dfa99e2fcdb16e0cb
hocaron <kkannu0407@gmail.com>
2022-07-08 20:14:41 +0900

    feat: score history entity 생성

---
+++ mashup-domain/src/main/java/kr/mashup/branding/domain/scoreHistory/ScoreHistory.java
@@ -0,0 +1,62 @@
+package kr.mashup.branding.domain.scoreHistory;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+import kr.mashup.branding.domain.BaseEntity;
+import kr.mashup.branding.domain.generation.Generation;
+import kr.mashup.branding.domain.member.Member;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ScoreHistory extends BaseEntity {
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Title title;
+
+    @NotNull
+    private String scheduleName;
+
+    @NotNull
+    private LocalDate date;
+
+    @NotNull
+    private Integer score;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "generation_id")
+    private Generation generation;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    public static ScoreHistory of(Title title, String scheduleName, LocalDate date, Integer score,
+        Generation generation, Member member
+    ) {
+        return new ScoreHistory(title, scheduleName, date, score, generation, member);
+    }
+
+    public ScoreHistory(Title title, String scheduleName, LocalDate date, Integer score,
+        Generation generation, Member member
+    ) {
+        this.title = title;
+        this.scheduleName = scheduleName;
+        this.date = date;
+        this.score = score;
+        this.generation = generation;
+        this.member = member;
+    }
+}
