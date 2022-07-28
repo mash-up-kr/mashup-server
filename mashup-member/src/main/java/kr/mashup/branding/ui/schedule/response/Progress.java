51370cc13ee7412c13f14e809388563be1cb8543
hocaron <kkannu0407@gmail.com>
2022-07-28 22:44:01 +0900

    feat: 스케줄 진행상황 enum 생성

---
+++ mashup-member/src/main/java/kr/mashup/branding/ui/schedule/response/Progress.java
@@ -0,0 +1,7 @@
+package kr.mashup.branding.ui.schedule.response;
+
+public enum Progress {
+    NOT_REGISTER, // 등록된 세미나가 없는 경우
+    ON_GOING, // 세미나가 존재하지만, 현재 날짜 이후로 등록된 세미나가 있는 경우
+    DONE; // 세미나가 존재하지만, 현재 날짜 이후로 등록된 세미나가 없는 경우
+}
