6cea7938da1ebbf4034ce9dea72d123e8c7623c1
hocaron <kkannu0407@gmail.com>
2022-07-31 14:03:26 +0900

    feat: 다음 세미나까지 daycount 메서드 생성

--- mashup-member/src/main/java/kr/mashup/branding/facade/schedule/ScheduleFacadeService.java
+++ mashup-member/src/main/java/kr/mashup/branding/facade/schedule/ScheduleFacadeService.java
@@ -1,5 +1,7 @@
 package kr.mashup.branding.facade.schedule;
 
+import java.time.LocalDateTime;
+import java.time.Period;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,7 +11,10 @@ import kr.mashup.branding.domain.generation.Generation;
 import kr.mashup.branding.domain.schedule.Schedule;
 import kr.mashup.branding.service.generation.GenerationService;
 import kr.mashup.branding.service.schedule.ScheduleService;
+import kr.mashup.branding.ui.schedule.response.Progress;
 import kr.mashup.branding.ui.schedule.response.ScheduleResponse;
+import kr.mashup.branding.ui.schedule.response.ScheduleResponseListWithProgress;
+import kr.mashup.branding.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -25,13 +30,51 @@ public class ScheduleFacadeService {
         return ScheduleResponse.from(schedule);
     }
 
-    public List<ScheduleResponse> getByGenerationNum(Integer number) {
+    public ScheduleResponseListWithProgress getByGenerationNum(Integer number) {
         Generation generation = generationService.getByNumberOrThrow(number);
 
         List<Schedule> scheduleList = scheduleService.getByGeneration(generation);
+        Progress progress;
+        Integer dateCount = calculateDateCount(scheduleList);
 
-        return scheduleList.stream()
+        if (scheduleList.size() == 0) {
+            progress = Progress.NOT_REGISTER;
+        } else if (dateCount == -1L) {
+            progress = Progress.DONE;
+        } else {
+            progress = Progress.ON_GOING;
+        }
+
+        List<ScheduleResponse> scheduleResponseList = scheduleList.stream()
             .map(ScheduleResponse::from)
             .collect(Collectors.toList());
+
+        return ScheduleResponseListWithProgress.of(progress, dateCount, scheduleResponseList);
+    }
+
+    private Integer calculateDateCount(List<Schedule> scheduleList) {
+        LocalDateTime currentTime = LocalDateTime.now();
+        Integer dateCount = -1;
+
+        int listSize = scheduleList.size();
+        for (int i = listSize - 1; i > 0; i--) {
+            LocalDateTime startedAt = scheduleList.get(i).getStartedAt();
+            LocalDateTime endedAt = scheduleList.get(i).getEndedAt();
+
+            if (isFinishedSchedule(currentTime, endedAt) || isOngoingSchedule(startedAt, endedAt, currentTime)) {
+                break;
+            }
+            dateCount = Period.between(currentTime.toLocalDate(), startedAt.toLocalDate()).getDays();
+        }
+
+        return dateCount;
+    }
+
+    private Boolean isFinishedSchedule(LocalDateTime now, LocalDateTime endedAt) {
+        return now.isAfter(endedAt);
+    }
+
+    private Boolean isOngoingSchedule(LocalDateTime startedAt, LocalDateTime endedAt, LocalDateTime now) {
+        return DateUtil.isInTime(startedAt, endedAt, now);
     }
 }
