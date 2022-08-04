f4299d637534b648be971d32c40d231e5ecf3c43
hocaron <kkannu0407@gmail.com>
2022-08-05 02:50:32 +0900

    feat: score-history 계산 메서드 구현

--- mashup-domain/src/main/java/kr/mashup/branding/service/scorehistory/ScoreHistoryService.java
+++ mashup-domain/src/main/java/kr/mashup/branding/service/scorehistory/ScoreHistoryService.java
@@ -1,14 +1,20 @@
 package kr.mashup.branding.service.scorehistory;
 
-import kr.mashup.branding.domain.generation.Generation;
-import kr.mashup.branding.domain.member.Member;
-import kr.mashup.branding.domain.scorehistory.ScoreHistory;
-import kr.mashup.branding.repository.scorehistory.ScoreHistoryRepository;
-import lombok.RequiredArgsConstructor;
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import kr.mashup.branding.domain.attendance.Attendance;
+import kr.mashup.branding.domain.attendance.AttendanceStatus;
+import kr.mashup.branding.domain.generation.Generation;
+import kr.mashup.branding.domain.member.Member;
+import kr.mashup.branding.domain.schedule.Schedule;
+import kr.mashup.branding.domain.scorehistory.ScoreHistory;
+import kr.mashup.branding.domain.scorehistory.ScoreType;
+import kr.mashup.branding.repository.scorehistory.ScoreHistoryRepository;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @Transactional(readOnly = true)
@@ -36,5 +42,27 @@ public class ScoreHistoryService {
     @Transactional
     public void deleteById(Long id) {
         scoreHistoryRepository.deleteById(id);
+    }
+
+    public ScoreHistory calculateAttendanceToScoreHistory(List<Attendance> attendanceList, Member member,
+        Schedule schedule) {
+        String type = "ATTENDANCE";
+        ScoreType scoreType = ScoreType.ATTENDANCE;
+
+        if (attendanceList.size() == 0) {
+            type = "ABSENT";
+            scoreType = ScoreType.ABSENT;
+        }
+        for (Attendance attendance : attendanceList) {
+            if (attendance.getStatus() == AttendanceStatus.LATE) {
+                type = "LATE";
+                scoreType = ScoreType.LATE;
+                break;
+            }
+        }
+
+        return ScoreHistory.of(type, scoreType.getName(), scoreType.getScore(), LocalDateTime.now(), schedule.getName(),
+            member.getGeneration(),
+            member);
     }
 }
