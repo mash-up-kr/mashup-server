0010f310d5448e5f852c87238ee6884165032207
JOngnan <dirlawhddbs@gmail.com>
2022-08-04 22:06:32 +0900

    feat: 응답 모델에 설명 추가

--- mashup-member/src/main/java/kr/mashup/branding/ui/attendance/response/TotalAttendanceResponse.java
+++ mashup-member/src/main/java/kr/mashup/branding/ui/attendance/response/TotalAttendanceResponse.java
@@ -1,5 +1,6 @@
 package kr.mashup.branding.ui.attendance.response;
 
+import io.swagger.annotations.ApiModelProperty;
 import kr.mashup.branding.domain.member.Platform;
 import lombok.Getter;
 import lombok.Value;
@@ -9,16 +10,23 @@ import java.util.List;
 @Getter
 @Value(staticConstructor = "of")
 public class TotalAttendanceResponse {
+    @ApiModelProperty(required = true)
     List<PlatformInfo> platformInfos;
+    @ApiModelProperty(required = true, notes = "몇 부인지 나타내는 값 (1 -> 1부, 2 -> 2부)")
     Integer eventNum;
+    @ApiModelProperty(required = true)
     Boolean isEnd;
 
     @Getter
     @Value(staticConstructor = "of")
     public static class PlatformInfo {
+        @ApiModelProperty(required = true)
         Platform platform;
+        @ApiModelProperty(required = true, notes = "전체 인원수")
         Long totalCount;
+        @ApiModelProperty(required = true, notes = "출석 인원수")
         Long attendanceCount;
+        @ApiModelProperty(required = true, notes = "지각 인원수")
         Long lateCount;
     }
 }
