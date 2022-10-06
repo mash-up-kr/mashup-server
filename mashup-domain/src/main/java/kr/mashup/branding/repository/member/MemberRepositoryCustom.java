1465e6956adf47832f4f2ccb05bddf8e4453dece
kh0712 <kkkh0712@naver.com>
2022-10-06 21:01:11 +0900

    fix: 불필요한 코드 정리

--- mashup-domain/src/main/java/kr/mashup/branding/repository/member/MemberRepositoryCustom.java
+++ mashup-domain/src/main/java/kr/mashup/branding/repository/member/MemberRepositoryCustom.java
@@ -1,14 +1,11 @@
 package kr.mashup.branding.repository.member;
 
 import kr.mashup.branding.domain.generation.Generation;
-import kr.mashup.branding.domain.member.Member;
 import kr.mashup.branding.domain.member.Platform;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import static kr.mashup.branding.repository.member.MemberRepositoryCustomImpl.*;
+import static kr.mashup.branding.repository.member.MemberRepositoryCustomImpl.MemberScoreQueryResult;
 
 public interface MemberRepositoryCustom {
 
