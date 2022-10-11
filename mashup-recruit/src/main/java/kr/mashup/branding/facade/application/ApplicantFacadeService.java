7b0024ddac6c80c1bc2143386738c1c321a968a7
kh0712 <kkkh0712@naver.com>
2022-10-11 20:56:42 +0900

    refactor: recruit applicant assembler 제거

--- mashup-recruit/src/main/java/kr/mashup/branding/facade/application/ApplicantFacadeService.java
+++ mashup-recruit/src/main/java/kr/mashup/branding/facade/application/ApplicantFacadeService.java
@@ -2,7 +2,6 @@ package kr.mashup.branding.facade.application;
 
 import kr.mashup.branding.domain.applicant.Applicant;
 import kr.mashup.branding.service.applicant.ApplicantService;
-import kr.mashup.branding.ui.applicant.ApplicantAssembler;
 import kr.mashup.branding.ui.applicant.vo.ApplicantResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,11 +12,10 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ApplicantFacadeService {
     private final ApplicantService applicantService;
-    private final ApplicantAssembler applicantAssembler;
 
     @Transactional(readOnly = true)
     public ApplicantResponse getApplicant(Long applicantId) {
         Applicant applicant = applicantService.getApplicant(applicantId);
-        return applicantAssembler.toApplicantResponse(applicant);
+        return ApplicantResponse.from(applicant);
     }
 }
