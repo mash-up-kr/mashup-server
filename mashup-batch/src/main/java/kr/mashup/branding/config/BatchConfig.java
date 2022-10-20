f1a35a69342874c6e10b8bb2564a4d0eb9d863c2
hocaron <kkannu0407@gmail.com>
2022-10-20 23:22:02 +0900

    feat: 필요한 의존성 추가

--- mashup-batch/src/main/java/kr/mashup/branding/config/BatchConfig.java
+++ mashup-batch/src/main/java/kr/mashup/branding/config/BatchConfig.java
@@ -2,8 +2,12 @@ package kr.mashup.branding.config;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableBatchProcessing
+@EnableScheduling
+@EnableWebMvc
 @Configuration
 public class BatchConfig {
     public static final String SPRING_BATCH_JOB_NAMES = "spring.batch.job.names";
