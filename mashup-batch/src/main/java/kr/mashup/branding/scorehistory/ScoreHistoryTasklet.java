7ba944ecb7aad8b3970bcf4381bc53e534c7e5d0
hocaron <kkannu0407@gmail.com>
2022-10-26 21:07:15 +0900

    refactor: 코드 리뷰 반영

--- mashup-batch/src/main/java/kr/mashup/branding/scorehistory/ScoreHistoryTasklet.java
+++ mashup-batch/src/main/java/kr/mashup/branding/scorehistory/ScoreHistoryTasklet.java
@@ -1,6 +1,7 @@
 package kr.mashup.branding.scorehistory;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
@@ -8,7 +9,10 @@ import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.transaction.annotation.Transactional;
 
+import kr.mashup.branding.domain.attendance.Attendance;
+import kr.mashup.branding.domain.member.Member;
 import kr.mashup.branding.domain.schedule.Schedule;
+import kr.mashup.branding.domain.scorehistory.ScoreHistory;
 import kr.mashup.branding.repository.schedule.ScheduleRepository;
 import kr.mashup.branding.service.attendance.AttendanceService;
 import kr.mashup.branding.service.scorehistory.ScoreHistoryService;
@@ -28,8 +32,11 @@ public class ScoreHistoryTasklet implements Tasklet {
 		List<Schedule> schedules = scheduleRepository.findAllByIsCounted(false);
 
 		schedules.forEach(schedule -> {
-			attendanceService.getByScheduleAndGroupByMember(schedule).forEach((member, attendances) ->
-				scoreHistoryService.save(scoreHistoryService.createByAttendances(member, schedule, attendances)));
+			Map<Member, List<Attendance>> attendanceMap = attendanceService.getByScheduleAndGroupByMember(schedule);
+			attendanceMap.forEach((member, attendances) -> {
+				ScoreHistory scoreHistory = scoreHistoryService.createByAttendances(member, schedule, attendances);
+				scoreHistoryService.save(scoreHistory);
+			});
 			schedule.changeIsCounted(true);
 		});
 		return RepeatStatus.FINISHED;
