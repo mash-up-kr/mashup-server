98825b51a43f168cba38898c105fa09a7a8dbf58
hocaron <kkannu0407@gmail.com>
2022-10-26 21:25:04 +0900

    refactor: 코드 리뷰 반영

--- mashup-domain/src/main/java/kr/mashup/branding/config/QueryDslConfig.java
+++ mashup-domain/src/main/java/kr/mashup/branding/config/QueryDslConfig.java
@@ -1,21 +1,20 @@
 package kr.mashup.branding.config;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Configuration
 public class QueryDslConfig {
 
-	@PersistenceContext
-	private EntityManager entityManager;
+    @PersistenceContext
+    private EntityManager entityManager;
 
-	@Bean
-	public JPAQueryFactory jpaQueryFactory() {
-		return new JPAQueryFactory(entityManager);
-	}
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
 }
