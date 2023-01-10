de689c7d8341b079ffa1614aa758ba33dc0b5238
kh0712 <kkkh0712@naver.com>
2023-01-10 23:36:28 +0900

    feat: 이메일 정렬 에러 수정

--- mashup-domain/src/main/java/kr/mashup/branding/repository/emailnotification/EmailNotificationRepositoryCustomImpl.java
+++ mashup-domain/src/main/java/kr/mashup/branding/repository/emailnotification/EmailNotificationRepositoryCustomImpl.java
@@ -1,14 +1,22 @@
 package kr.mashup.branding.repository.emailnotification;
 
 import com.querydsl.core.QueryResults;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import kr.mashup.branding.domain.ResultCode;
 import kr.mashup.branding.domain.email.EmailNotification;
+import kr.mashup.branding.domain.exception.BadRequestException;
 import kr.mashup.branding.util.QueryUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static kr.mashup.branding.domain.adminmember.entity.QAdminMember.adminMember;
@@ -22,11 +30,15 @@ public class EmailNotificationRepositoryCustomImpl implements EmailNotificationR
 
     @Override
     public Page<EmailNotification> findBySearchWord(Optional<String> searchWord, Pageable pageable) {
+
+        final Sort sort = pageable.getSortOr(Sort.by(Sort.Direction.DESC, "createdAt"));
+
         QueryResults<EmailNotification> results = queryFactory
             .selectFrom(emailNotification)
             .innerJoin(emailNotification.sender, adminMember).fetchJoin()
             .innerJoin(emailNotification.generation, generation).fetchJoin()
             .where(isContainSearchWord(searchWord))
+            .orderBy(getOrderSpecifier(sort))
             .fetchResults();
 
         return QueryUtils.toPage(results, pageable);
@@ -39,5 +51,29 @@ public class EmailNotificationRepositoryCustomImpl implements EmailNotificationR
 
         final String searchWordParam = searchWord.get();
         return emailNotification.memo.contains(searchWordParam);
+    }
+
+    private OrderSpecifier[] getOrderSpecifier(Sort sort) {
+
+        final List<OrderSpecifier> orderSpecifiers = new ArrayList<>();
+        
+        for (Sort.Order order : sort) {
+            final Sort.Direction direction = order.getDirection();
+            final String field = order.getProperty();
+            final Order qOrder = direction.isAscending() ? Order.ASC : Order.DESC;
+
+            OrderSpecifier orderSpecifier = null;
+
+            if (field.equals("createdAt")) {
+                orderSpecifier = new OrderSpecifier(qOrder, Expressions.path(Object.class, emailNotification, field));
+            } else if (field.equals("templateName")) {
+                orderSpecifier = new OrderSpecifier(qOrder, Expressions.path(Object.class, emailNotification, field));
+            } else {
+                throw new BadRequestException(ResultCode.BAD_REQUEST);
+            }
+
+            orderSpecifiers.add(orderSpecifier);
+        }
+        return orderSpecifiers.toArray(new OrderSpecifier[0]);
     }
 }
