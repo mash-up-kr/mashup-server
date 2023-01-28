eb6dfd54f1994752ff941a51ba257616cd25358b
JOngnan <dirlawhddbs@gmail.com>
2023-01-28 18:21:31 +0900

    fix: 배치 비활성화 수정

--- mashup-batch/src/main/java/kr/mashup/branding/MashupBatchApplication.java
+++ mashup-batch/src/main/java/kr/mashup/branding/MashupBatchApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MashupBatchApplication {
     public static void main(String[] args) {
-        System.exit(SpringApplication.exit(
-            SpringApplication.run(MashupBatchApplication.class, args)
-        ));
+        System.setProperty("spring.config.name", "application,batch");
+        System.exit(
+            SpringApplication.exit(
+                SpringApplication.run(MashupBatchApplication.class, args)
+            )
+        );
     }
 }
