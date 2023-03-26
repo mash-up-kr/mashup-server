6d4d19a624635004d92e48afaece4db60faefc4d
alertjjm <sdr2936@gmail.com>
2023-03-27 01:17:52 +0900

    조인칼럼 추가

--- mashup-domain/src/main/java/kr/mashup/branding/domain/danggn/DanggnShakeLog.java
+++ mashup-domain/src/main/java/kr/mashup/branding/domain/danggn/DanggnShakeLog.java
@@ -19,7 +19,8 @@ public class DanggnShakeLog {
     @GeneratedValue
     private Long id;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "member_generation_id")
     private MemberGeneration memberGeneration;
 
     private Long shakeScore;
