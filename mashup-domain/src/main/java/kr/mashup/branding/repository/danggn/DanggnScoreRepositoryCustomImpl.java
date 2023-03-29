c35dc4716d6a292b31460b1526c300aef79ed1b8
alertjjm <sdr2936@gmail.com>
2023-03-29 21:39:14 +0900

    refactor code

--- mashup-domain/src/main/java/kr/mashup/branding/repository/danggn/DanggnScoreRepositoryCustomImpl.java
+++ mashup-domain/src/main/java/kr/mashup/branding/repository/danggn/DanggnScoreRepositoryCustomImpl.java
@@ -1,14 +1,13 @@
 package kr.mashup.branding.repository.danggn;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import kr.mashup.branding.domain.danggn.DanggnScore;
 import kr.mashup.branding.domain.member.Platform;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static kr.mashup.branding.domain.danggn.QDanggnScore.danggnScore;
 import static kr.mashup.branding.domain.generation.QGeneration.generation;
@@ -33,35 +32,29 @@ public class DanggnScoreRepositoryCustomImpl implements DanggnScoreRepositoryCus
     public List<DanggnScorePlatformQueryResult> findOrderedDanggnScorePlatformListByGenerationNum(Integer generationNumber) {
         return queryFactory
             .select(
-                memberGeneration.platform,
-                danggnScore.totalShakeScore.sum(),
-                danggnScore.lastShakedAt.max()
+                Projections.fields(DanggnScorePlatformQueryResult.class,
+                    memberGeneration.platform.as("platform"),
+                    danggnScore.totalShakeScore.sum().as("totalScore")
+                )
             )
             .from(danggnScore)
             .innerJoin(danggnScore.memberGeneration, memberGeneration)
+            .innerJoin(memberGeneration.generation, generation)
+            .on(generation.number.eq(generationNumber))
             .groupBy(memberGeneration.platform)
             .orderBy(
                 danggnScore.totalShakeScore.sum().desc(),
                 danggnScore.lastShakedAt.max().asc()
             )
-            .fetch()
-            .stream()
-            .map(tuple -> new DanggnScorePlatformQueryResult(
-                tuple.get(memberGeneration.platform),
-                tuple.get(danggnScore.totalShakeScore.sum()),
-                tuple.get(danggnScore.lastShakedAt.max())
-            ))
-            .collect(Collectors.toList());
+            .fetch();
     }
 
     @Getter
     @RequiredArgsConstructor
     public static class DanggnScorePlatformQueryResult {
-        private final Platform platform;
+        Platform platform;
 
-        private final Long totalScore;
-
-        private final LocalDateTime lastShakedAt;
+        Long totalScore;
     }
 }
 
