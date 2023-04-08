f3631ba5fbd125d8e9efa6b573930e3563c14203
hocaron <kkannu0407@gmail.com>
2023-04-08 16:41:52 +0900

    fix: 오타 수정

--- mashup-member/src/main/java/kr/mashup/branding/ui/member/request/PushNotificationRequest.java
+++ mashup-member/src/main/java/kr/mashup/branding/ui/member/request/PushNotificationRequest.java
@@ -10,6 +10,6 @@ public class PushNotificationRequest {
     @NotNull(message = "매시업 소식 알림 동의 여부는 필수값입니다.")
     private Boolean newsPushNotificationAgreed;
 
-    @NotNull(message = "당근ㄴ 흔들기 알림 동의 여부는 필수값입니다.")
+    @NotNull(message = "당근 흔들기 알림 동의 여부는 필수값입니다.")
     private Boolean danggnPushNotificationAgreed;
 }
