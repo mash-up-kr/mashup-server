2cd1bd400f65874b15838f829404ccad9ba7b4a8
hocaron <kkannu0407@gmail.com>
2023-04-12 14:02:35 +0900

    fix: 완료된 스케줄만 점수 계산 배치가 실행되도록 수정

--- mashup-domain/src/main/java/kr/mashup/branding/repository/schedule/ScheduleRepository.java
+++ mashup-domain/src/main/java/kr/mashup/branding/repository/schedule/ScheduleRepository.java
@@ -5,12 +5,13 @@ import kr.mashup.branding.domain.schedule.Schedule;
 import kr.mashup.branding.domain.schedule.ScheduleStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long>, ScheduleRepositoryCustom {
 
-    List<Schedule> findAllByIsCounted(Boolean isCounted);
+    List<Schedule> findAllByIsCountedAndEndedAtIsBefore(Boolean isCounted, LocalDateTime at);
 
     Optional<Schedule> findByIdAndStatus(Long id, ScheduleStatus status);
 
