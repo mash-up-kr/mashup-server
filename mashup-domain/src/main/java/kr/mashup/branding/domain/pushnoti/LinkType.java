8f8ad81d7f62eab86e670710b935e206d1bc8f32
hocaron <kkannu0407@gmail.com>
2023-04-23 18:12:51 +0900

    fix: 인덴트 수정

--- mashup-domain/src/main/java/kr/mashup/branding/domain/pushnoti/LinkType.java
+++ mashup-domain/src/main/java/kr/mashup/branding/domain/pushnoti/LinkType.java
@@ -2,8 +2,8 @@ package kr.mashup.branding.domain.pushnoti;
 
 public enum LinkType {
     MAIN,       // 기본 페이지
-    QR,           // QR 페이지
-    DANGGN,   // 당근 페이지
-    MYPAGE,   // 마이 페이지
+    QR,         // QR 페이지
+    DANGGN,     // 당근 페이지
+    MYPAGE,     // 마이 페이지
     ;
 }
