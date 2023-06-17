f0309bc64c267d1eb9638ad9c9c4e0aa5a0e7e99
hocaron <kkannu0407@gmail.com>
2023-06-18 02:52:58 +0900

    feat: 당근 랭킹 회차 및 리워드 엔티티 생성

---
+++ mashup-domain/src/main/java/kr/mashup/branding/domain/danggn/DanggnRankingReward.java
@@ -0,0 +1,29 @@
+package kr.mashup.branding.domain.danggn;
+
+import javax.persistence.Entity;
+
+import kr.mashup.branding.domain.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DanggnRankingReward extends BaseEntity {
+
+	private String comment;
+
+	private Long firstPlaceRecordMemberId;
+
+	private Long danggnRankingRoundId;
+
+	public static DanggnRankingReward from(Long firstPlaceRecordMemberGenerationId, Long danggnRankingRoundId) {
+		return new DanggnRankingReward(firstPlaceRecordMemberGenerationId, danggnRankingRoundId);
+	}
+
+	private DanggnRankingReward(Long firstPlaceRecordMemberId, Long danggnRankingRoundId) {
+		this.firstPlaceRecordMemberId = firstPlaceRecordMemberId;
+		this.danggnRankingRoundId = danggnRankingRoundId;
+	}
+}
