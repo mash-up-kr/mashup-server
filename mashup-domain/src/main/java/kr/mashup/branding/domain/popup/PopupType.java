34aeb9ae83044d4f745afec92667f15841073238
hocaron <kkannu0407@gmail.com>
2023-07-29 22:59:19 +0900

    fix: 당근 업데이트 팝업 노출 기간 수정

--- mashup-domain/src/main/java/kr/mashup/branding/domain/popup/PopupType.java
+++ mashup-domain/src/main/java/kr/mashup/branding/domain/popup/PopupType.java
@@ -1,19 +1,19 @@
 package kr.mashup.branding.domain.popup;
 
+import kr.mashup.branding.util.DateUtil;
+import lombok.AllArgsConstructor;
+
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import kr.mashup.branding.util.DateUtil;
-import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public enum PopupType {
 
 	DANGGN_REWARD(LocalDate.MIN, LocalDate.MAX),
 	DANGGN(LocalDate.of(2023, 5, 18), LocalDate.of(2023, 6, 2)),			// 당근 흔들기 릴리즈 팝업 타입
-	DANGGN_UPDATE(LocalDate.of(2023, 6, 26), LocalDate.of(2023, 7, 26)), 	// 당근 흔들기 업데이트 팝업 타입 FIXME: 배포 일자에 맞춰서, 노출 일자 수정 필요
+	DANGGN_UPDATE(LocalDate.of(2023, 6, 26), LocalDate.of(2023, 8, 26)), 	// 당근 흔들기 업데이트 팝업 타입 FIXME: 배포 일자에 맞춰서, 노출 일자 수정 필요
 	;
 
 	private final LocalDate startedDate;
