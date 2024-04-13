ae3168b4ba9597df671f732fc3d8a8eec607a2f8
hocaron <kkannu0407@gmail.com>
2024-04-14 00:45:17 +0900

    fix: job meta table 사용하지 않도록 변경

--- mashup-batch/src/main/java/kr/mashup/branding/config/BatchConfig.java
+++ mashup-batch/src/main/java/kr/mashup/branding/config/BatchConfig.java
@@ -1,14 +1,22 @@
 package kr.mashup.branding.config;
 
+import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import javax.sql.DataSource;
+
 @EnableBatchProcessing
 @EnableScheduling
 @EnableWebMvc
 @Configuration
-public class BatchConfig {
+public class BatchConfig extends DefaultBatchConfigurer {
     public static final String SPRING_BATCH_JOB_NAMES = "spring.batch.job.names";
+
+    // spring batch 5 로 버전 업데이트시에 대응 필요
+    @Override
+    public void setDataSource(DataSource dataSource) {
+    }
 }
