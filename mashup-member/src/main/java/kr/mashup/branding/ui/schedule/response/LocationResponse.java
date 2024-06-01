aefea746e6dd3e1103d1821b0bccff4036eef5bd
eunjungL <dldmswjd322@gmail.com>
2024-06-01 15:49:45 +0900

    fix: LocationResponse에 detail/roadAddress도 같이 내려주게 수정

--- mashup-member/src/main/java/kr/mashup/branding/ui/schedule/response/LocationResponse.java
+++ mashup-member/src/main/java/kr/mashup/branding/ui/schedule/response/LocationResponse.java
@@ -10,18 +10,24 @@ public class LocationResponse {
     private final Double longitude;
     private final String address;
     private final String placeName;
+    private final String roadAddress;
+    private final String detailAddress;
 
-    public LocationResponse(Double latitude, Double longitude, String address, String placeName) {
+    public LocationResponse(Double latitude, Double longitude, String address, String placeName, String roadAddress, String detailAddress) {
         this.latitude = latitude;
         this.longitude = longitude;
         this.address = address;
         this.placeName = placeName;
+        this.roadAddress = roadAddress;
+        this.detailAddress = detailAddress;
     }
 
     public static LocationResponse from(Location location) {
         return new LocationResponse(
                 location.getLatitude(),
                 location.getLongitude(),
+                location.getRoadAddress(),
+                location.getDetailAddress(),
                 location.getRoadAddress(),
                 location.getDetailAddress()
         );
