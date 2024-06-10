e0d05868d623196a61067c043b40ae0d8dc06271
hocaron <kkannu0407@gmail.com>
2024-06-10 23:28:00 +0900

    feat: 출석점수 집계 배치를 스케줄러로 변경 및 수동 api 추가

--- mashup-admin/src/main/java/kr/mashup/branding/facade/scorehistory/ScoreHistoryProcessor.java
+++ mashup-admin/src/main/java/kr/mashup/branding/facade/scorehistory/ScoreHistoryProcessor.java
@@ -1,16 +1,20 @@
-package kr.mashup.branding.facade;
+package kr.mashup.branding.facade.scorehistory;
 
 import kr.mashup.branding.domain.attendance.Attendance;
 import kr.mashup.branding.domain.member.Member;
+import kr.mashup.branding.domain.pushnoti.vo.SeminarAttendanceAppliedVo;
 import kr.mashup.branding.domain.schedule.Schedule;
 import kr.mashup.branding.domain.schedule.ScheduleType;
 import kr.mashup.branding.domain.scorehistory.ScoreHistory;
 import kr.mashup.branding.domain.scorehistory.ScoreType;
+import kr.mashup.branding.infrastructure.pushnoti.PushNotiEventPublisher;
 import kr.mashup.branding.service.attendance.AttendanceService;
 import kr.mashup.branding.service.member.MemberService;
 import kr.mashup.branding.service.schedule.ScheduleService;
 import kr.mashup.branding.service.scorehistory.ScoreHistoryService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,20 +23,22 @@ import java.util.*;
 
 @Service
 @RequiredArgsConstructor
-public class ScoreHistoryFacadeService {
+@Slf4j
+public class ScoreHistoryProcessor {
 
     private final ScheduleService scheduleService;
     private final AttendanceService attendanceService;
     private final ScoreHistoryService scoreHistoryService;
     private final MemberService memberService;
 
+    private final PushNotiEventPublisher pushNotiEventPublisher;
+
     /**
      * 출석점수 집계를 실행합니다.
-     *
-     * @return 출석점수가 업데이트된 member 리스트
      */
+    @Scheduled(cron = "0 0 17 ? * SUN") // 매주 일요일 오후 5시
     @Transactional
-    public List<Member> create() {
+    public void create() {
         List<Schedule> schedules = scheduleService.findEndedScheduleByIsCountedAndScheduleType(false, ScheduleType.ALL);
         Set<Member> updatedMember = new HashSet<>();
 
@@ -47,15 +53,14 @@ public class ScoreHistoryFacadeService {
 
             schedule.changeIsCounted(true);
         });
-
-        return List.copyOf(updatedMember);
+        pushNotiEventPublisher.publishPushNotiSendEvent(new SeminarAttendanceAppliedVo(List.copyOf(updatedMember)));
     }
 
     /**
      * attendance 가 있는 member 의 출석점수를 계산합니다.
      *
      * @param schedule 출석점수 집계할 스케줄
-     * @param members 출석체크한 멤버
+     * @param members  출석체크한 멤버
      * @return 출석점수 리스트
      */
     private List<ScoreHistory> getCheckedAttendance(Schedule schedule, List<Member> members) {
@@ -73,7 +78,7 @@ public class ScoreHistoryFacadeService {
      * attendance 가 없는 member 의 출석점수(결석처리)를 계산합니다.
      *
      * @param schedule 출석점수 집계할 스케줄
-     * @param members 출석체크를 한번도 하지않은 멤버
+     * @param members  출석체크를 한번도 하지않은 멤버
      * @return 출석점수 리스트
      */
     private List<ScoreHistory> getUnCheckedAttendance(Schedule schedule, List<Member> members) {
@@ -84,11 +89,10 @@ public class ScoreHistoryFacadeService {
         return scoreHistories;
     }
 
-
     /**
      * schedule 의 startDate 기준으로 출석점수 집계 결과를 삭제합니다.
      *
-     * @param scheduleStartDate
+     * @param scheduleStartDate 스케줄 시작 일자
      */
     public void delete(LocalDate scheduleStartDate) {
         Schedule schedule = scheduleService.findScheduleByStartDateAndScheduleType(scheduleStartDate, ScheduleType.ALL);
