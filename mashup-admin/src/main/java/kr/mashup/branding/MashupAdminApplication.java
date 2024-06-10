4361f0b151808f542cd68984bccf3c6c76a212f2
hocaron <kkannu0407@gmail.com>
2024-06-10 23:28:00 +0900

    feat: 출석점수 집계 배치를 스케줄러로 변경 및 수동 api 추가

--- mashup-admin/src/main/java/kr/mashup/branding/MashupAdminApplication.java
+++ mashup-admin/src/main/java/kr/mashup/branding/MashupAdminApplication.java
@@ -2,8 +2,10 @@ package kr.mashup.branding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MashupAdminApplication {
     public static void main(String[] args) {
         System.setProperty("spring.config.name", "application,admin");
