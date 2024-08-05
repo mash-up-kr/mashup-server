1b383517f0c5c82e3d68a9a2e650c3a6741f865a
hocaron <kkannu0407@gmail.com>
2024-08-05 23:33:05 +0900

    fix: 월 일만 비교하도록 변경

--- mashup-domain/src/main/java/kr/mashup/branding/service/member/MemberBirthdayDto.java
+++ mashup-domain/src/main/java/kr/mashup/branding/service/member/MemberBirthdayDto.java
@@ -4,12 +4,20 @@ import kr.mashup.branding.domain.member.Platform;
 import lombok.Getter;
 
 import java.time.LocalDate;
+import java.time.MonthDay;
 
 @Getter
 public class MemberBirthdayDto {
 
-    private Long memberId;
-    private String name;
-    private Platform platform;
-    private LocalDate birthDate;
+    private final Long memberId;
+    private final String name;
+    private final Platform platform;
+    private final MonthDay birthDate;
+
+    public MemberBirthdayDto(Long memberId, String name, Platform platform, LocalDate birthDate) {
+        this.memberId = memberId;
+        this.name = name;
+        this.platform = platform;
+        this.birthDate = MonthDay.from(birthDate);
+    }
 }
