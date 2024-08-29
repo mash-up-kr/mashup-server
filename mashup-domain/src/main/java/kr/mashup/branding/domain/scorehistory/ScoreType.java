5a167f3049a8400aeba365993e28b4e5eacac963
KyungHwanKim <kyung.hwan-kim@linecorp.com>
2024-08-29 11:35:48 +0900

    add score type

--- mashup-domain/src/main/java/kr/mashup/branding/domain/scorehistory/ScoreType.java
+++ mashup-domain/src/main/java/kr/mashup/branding/domain/scorehistory/ScoreType.java
@@ -11,15 +11,22 @@ public enum ScoreType {
     ABSENT("전체 세미나 결석", -1.0),
     LATE("전체 세미나 지각", -0.5),
     DEPLOY_FAILURE("프로젝트 배포 실패", -0.5),
+    @Deprecated
     DEPLOY_SUCCESS("프로젝트 배포 성공", 1.0),
     SEMINAR_PRESENTATION("전체 세미나 발표", 1.0),
+    @Deprecated
     TECH_BLOG_WRITE("기술블로그 작성", 1.0),
     MASHUP_CONTENT_WRITE("Mah-Up 콘텐츠 글 작성", 0.5),
-    PROJECT_LEADER("프로젝트팀 팀장", 2.0),
+    PROJECT_LEADER("프로젝트 팀 리더", 2.0),
+    @Deprecated
     PROJECT_SUBLEADER("프로젝트 부팀장", 2.0),
     HACKATHON_COMMITTEE("해커톤 준비 위원회", 1.0),
-    MASHUP_STAFF("스태프", 99.0),
+    ADD_SCORE_DURING_SEMINAR_ACTIVITY_0_5("세미나 활동 중 추가 점수", 0.5),
+    ADD_SCORE_DURING_SEMINAR_ACTIVITY_1("세미나 활동 중 추가 점수", 1d),
+    MASHUP_STAFF("스태프", 3d),
+    @Deprecated
     MASHUP_LEADER("회장", 100.0),
+    @Deprecated
     MASHUP_SUBLEADER("부회장", 100.0),
     ETC("기타", 0.0),
     DEFAULT("기본 점수", 3.0),
