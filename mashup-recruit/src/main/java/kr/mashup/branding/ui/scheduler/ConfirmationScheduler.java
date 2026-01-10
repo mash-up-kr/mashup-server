adc6de0687aee4f0e10cb33ced6084784b7aab79
khkim <kkkh0712@naver.com>
2026-01-10 12:13:05 +0900

    fix time and log format

--- mashup-recruit/src/main/java/kr/mashup/branding/ui/scheduler/ConfirmationScheduler.java
+++ mashup-recruit/src/main/java/kr/mashup/branding/ui/scheduler/ConfirmationScheduler.java
@@ -17,10 +17,10 @@ public class ConfirmationScheduler {
      * 지원 마감시 임시저장인 지원서에 대한 지원자 응답 변경
      * TO_BE_DETERMINED -> NOT_APPLICABLE
      */
-    @Scheduled(cron = "0 0 0 17 2 ?") // 2025-02-17 00:00:00
+    @Scheduled(cron = "0 0 0 12 2 ?") // 서류 마감 26년 2월11일 -> 2월 12일 00시에 변경
     public void updateConfirmationAtRecruitmentEnded() {
         log.info("[ConfirmationScheduler] updateConfirmationAtRecruitmentEnded >>> start");
-        confirmationFacadeService.updateToBeDeterminedToNotApplicable(15);
+        confirmationFacadeService.updateToBeDeterminedToNotApplicable(16);
         log.info("[ConfirmationScheduler] updateConfirmationAtRecruitmentEnded >>> end");
     }
 
@@ -28,10 +28,10 @@ public class ConfirmationScheduler {
      * 서류 발표 후 24시간 뒤 서류 합격이지만 응답 대기중인 지원서에 대한 지원자 응답 변경
      * INTERVIEW_CONFIRM_WAITING -> INTERVIEW_CONFIRM_REJECTED
      */
-    @Scheduled(cron = "0 0 21 20 2 ?") // 2025-02-19 21:00:00 24시간 뒤인 02-20 21:00:00
+    @Scheduled(cron = "0 0 00 20 2 ?") // 서류 발표 26-02-18일 다음 날이 끝나는 2월 20일 00시
     public void updateConfirmationAtScreeningEnded() {
         log.info("[ConfirmationScheduler] updateConfirmationAtScreeningEnded >>> start");
-        confirmationFacadeService.updateInterviewConfirmWaitingToRejected(15);
+        confirmationFacadeService.updateInterviewConfirmWaitingToRejected(16);
         log.info("[ConfirmationScheduler] updateConfirmationAtScreeningEnded >>> end");
     }
 
@@ -40,10 +40,10 @@ public class ConfirmationScheduler {
      * 15기 한정 24시간이 아닌 23:59:59 로 변경
      * FINAL_CONFIRM_WAITING -> FINAL_CONFIRM_REJECTED
      */
-    @Scheduled(cron = "0 0 00 26 2 ?") // 2025-02-24 21:00:00 24시간 뒤인 02-25 21:00:00
+    @Scheduled(cron = "0 0 00 28 2 ?") // 26-02-26 최종 발표 하고 2월 28일 00시
     public void updateConfirmationAtInterviewEnded() {
         log.info("[ConfirmationScheduler] updateConfirmationAtInterviewEnded >>> start");
-        confirmationFacadeService.updateFinalConfirmWaitingToRejected(15);
+        confirmationFacadeService.updateFinalConfirmWaitingToRejected(16);
         log.info("[ConfirmationScheduler] updateConfirmationAtInterviewEnded >>> end");
     }
 }
